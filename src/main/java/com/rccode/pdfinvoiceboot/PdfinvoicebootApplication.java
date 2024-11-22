673e396d38b5732f8633a30f474f50e7492b1c35
Rahul Chauhan <crahul10101991@gmail.com>
2024-11-22 11:14:45 +0530

    adding project

---
+++ src/main/java/com/rccode/pdfinvoiceboot/PdfinvoicebootApplication.java
@@ -0,0 +1,13 @@
+package com.rccode.pdfinvoiceboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfinvoicebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfinvoicebootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rccode/pdfinvoiceboot/PdfinvoicebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rccode.pdfinvoiceboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfinvoicebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
